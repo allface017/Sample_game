6642bb86f66de6ac18ee51292420795b6226e87c
allface017 <113155656+allface017@users.noreply.github.com>
2023-12-19 14:33:56 +0900

    太郎と花子を表示させた。カーソルキーで移動させた close #4, #5, #6

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -28,11 +28,35 @@ class MainScene extends Phaser.Scene {
     this.taro = taro;
     this.hanako = hanako;
     }
-
+    arrow_move(cursors, object1,object2){
+        if(cursors.up.isDown){
+            console.log("Up!!");
+            object1.setVelocityY(-40);// 上方向の速度を設定
+            object2.setVelocityY(40);// 上方向の速度を設定
+        }else if(cursors.down.isDown){
+            console.log("down!!");
+            object1.setVelocityY(40);// 下方向の速度を設定
+            object2.setVelocityY(-40);// 下方向の速度を設定
+        }else if(cursors.left.isDown){
+            console.log("Left");
+            object1.setVelocityX(-40);// 左方向の速度を設定
+            object2.setVelocityX(40);// 左方向の速度を設定
+        }else if(cursors.right.isDown){
+            console.log("Right!!");
+            object1.setVelocityX(40);// 右方向の速度を設定
+            object2.setVelocityX(-40);// 右方向の速度を設定
+        }else{
+            object1.setVelocity(0,0);// 横方向の速度を0
+            object2.setVelocity(0,0);// 横方向の速度を0
+        }
+    }
 
 
     // 毎フレーム実行される繰り返し処理
     update() {
+        let cursors = this.input.keyboard.createCursorKeys();
 
+        // 矢印キーのカーソル情報とplayerスプライトをarrow_move()メソッドに渡す
+        this.arrow_move(cursors, this.taro,this.hanako);
     }
 }
